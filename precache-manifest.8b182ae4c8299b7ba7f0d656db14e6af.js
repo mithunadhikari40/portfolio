a839f2791981810ff731d9e53ca131241a39836a
Mithun Adhikari <mithunadhikari40@gmail.com>
2021-01-10 19:24:10 +0545

    Updates

---
+++ precache-manifest.8b182ae4c8299b7ba7f0d656db14e6af.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c697da3b422b0c3b396ccf70a25d61e",
+    "url": "/mithunadhikari40/portfolio/index.html"
+  },
+  {
+    "revision": "13d10cbedfe7d2b4126a",
+    "url": "/mithunadhikari40/portfolio/static/css/3.00373544.chunk.css"
+  },
+  {
+    "revision": "870dd3d23d9d4811a7ee",
+    "url": "/mithunadhikari40/portfolio/static/css/4.a922a401.chunk.css"
+  },
+  {
+    "revision": "c68fa7dc67f7e538dbb5",
+    "url": "/mithunadhikari40/portfolio/static/css/main.c75aa318.chunk.css"
+  },
+  {
+    "revision": "accb2ec51863d584ea3a",
+    "url": "/mithunadhikari40/portfolio/static/js/2.65a7f324.chunk.js"
+  },
+  {
+    "revision": "0a6d64cd3b49be6a14afdf21fa7c6d43",
+    "url": "/mithunadhikari40/portfolio/static/js/2.65a7f324.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d10cbedfe7d2b4126a",
+    "url": "/mithunadhikari40/portfolio/static/js/3.329758d6.chunk.js"
+  },
+  {
+    "revision": "870dd3d23d9d4811a7ee",
+    "url": "/mithunadhikari40/portfolio/static/js/4.610e0b8e.chunk.js"
+  },
+  {
+    "revision": "c68fa7dc67f7e538dbb5",
+    "url": "/mithunadhikari40/portfolio/static/js/main.3accaff5.chunk.js"
+  },
+  {
+    "revision": "773cc6298d58d26adbae",
+    "url": "/mithunadhikari40/portfolio/static/js/runtime-main.7b816558.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/mithunadhikari40/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/mithunadhikari40/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "82ecfe56f4f144dab55c1859bdb09fe7",
+    "url": "/mithunadhikari40/portfolio/static/media/broadway_logo.82ecfe56.jpg"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/mithunadhikari40/portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/mithunadhikari40/portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/mithunadhikari40/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ce27c9684385e35936d5ea187c2077b",
+    "url": "/mithunadhikari40/portfolio/static/media/g1k_pic.8ce27c96.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/mithunadhikari40/portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/mithunadhikari40/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "15cbf391f2bc1d66e4b4622656401b0f",
+    "url": "/mithunadhikari40/portfolio/static/media/ncit_logo.15cbf391.jpg"
+  },
+  {
+    "revision": "829ad0a1deef4d7295adfecdf12df2be",
+    "url": "/mithunadhikari40/portfolio/static/media/nextuLogo.829ad0a1.webp"
+  },
+  {
+    "revision": "920b2f34a8e9f331af7d4756b17d4f11",
+    "url": "/mithunadhikari40/portfolio/static/media/post_flood.920b2f34.jpg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/mithunadhikari40/portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "e35e14a02511471fbe7189f4bce70fe0",
+    "url": "/mithunadhikari40/portfolio/static/media/rims_house.e35e14a0.jpg"
+  },
+  {
+    "revision": "25018d29d5ab115bc51c3e9a2d92f90f",
+    "url": "/mithunadhikari40/portfolio/static/media/saayaHealthLogo.25018d29.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/mithunadhikari40/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/mithunadhikari40/portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
